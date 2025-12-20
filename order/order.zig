73fdbb5d386b6b11f0501d213f2da03e075a18f2
koziolek24 <kozlowski11maciej@gmail.com>
2025-12-21 00:59:18 +0100

    added constructor to order

--- order/order.zig
+++ order/order.zig
@@ -13,4 +13,16 @@ pub const Order = struct {
     volume: u64,
     side: OrderSide,
     timestamp: i64,
+
+    pub fn init(orderID: u64, ownerID: u64, stockID: u64, price: u64, volume: u64, side: OrderSide) Order {
+        return Order{
+            .orderID = orderID,
+            .ownerID = ownerID,
+            .stockID = stockID,
+            .price = price,
+            .volume = volume,
+            .side = side,
+            .timestamp = std.time.milliTimestamp(),
+        };
+    }
 };
