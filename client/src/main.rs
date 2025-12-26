22555c310ff4e220c8b1cfee5ff2d3f1b5206a5c
Bartosz Kozlowski <bartikozlowski@gmail.com>
2025-12-26 22:23:38 +0100

    simple tcp connection

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,12 @@
+use tokio::{io::AsyncReadExt, io::AsyncWriteExt, net::TcpStream};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let mut stream = TcpStream::connect("127.0.0.1:8080")
+        .await
+        .expect("Failed to connect to server");
+    stream.write("hello".as_bytes()).await.unwrap();
+    let mut input_buffer = [0; 1024];
+    stream.read(&mut input_buffer).await.unwrap();
+    println!("{}", String::from_utf8_lossy(&input_buffer));
 }
--- master/src/main.rs
+++ master/src/main.rs
@@ -1,4 +1,19 @@
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    println!("Server started on 127.0.0.1:8080");
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        tokio::spawn(handle_client(stream));
+    }
+}
+
+async fn handle_client(mut stream: TcpStream) {
+    let mut input_buffer = [0; 1024];
+    stream.read(&mut input_buffer).await.unwrap();
+    let response = "hello";
+    stream.write(response.as_bytes()).await.unwrap();
 }
